b05ac4e48dd45bc49e9ddde21bf4de23734432d3
Ryan G <ryan@gamepanelx.com>
2013-04-03 19:43:37 -0700

    Added 2 new languages

---
+++ languages/croatian.php
@@ -0,0 +1,197 @@
+<?php
+/*
+ * GamePanelX
+ * Hrvatski prijevod by Slay
+ * kiki.stipic@gmail.com
+*/
+
+$lang = array();
+
+$lang['yes']                = 'Da';
+$lang['no']                 = 'Ne';
+$lang['active']             = 'Aktivan';
+$lang['inactive']           = 'Neaktivan';
+$lang['setup']              = 'Postavljanje';
+$lang['settings']           = 'Postavke';
+$lang['language']           = 'Jezici';
+$lang['default_language']   = 'Zadani jezik';
+$lang['email_address']      = 'Email Adrese';
+$lang['company']            = 'Kompanija';
+$lang['theme']              = 'Tema';
+$lang['save']               = 'Spremi';
+$lang['game']               = 'Igra';
+$lang['voice']              = 'Glas';
+$lang['desc']               = 'Opis';
+$lang['status']             = 'Status';
+$lang['manage']             = 'Upravljanje';
+$lang['info']               = 'Informacije';
+$lang['owner']              = 'Vlasnik';
+$lang['date_added']         = 'Datum dodavanja';
+$lang['last_updated']       = 'Zadnja nadogradnja';
+$lang['default']            = 'Zadano';
+$lang['delete']             = 'Pobriši';
+$lang['create']             = 'Kreiraj';
+$lang['add']                = 'Dodaj';
+$lang['edit']               = 'Uredi';
+$lang['failed']             = 'Neuspjelo';
+$lang['name']               = 'Ime';
+$lang['first_name']         = 'Ime';
+$lang['last_name']          = 'Prezime';
+$lang['type']               = 'Vrsta';
+$lang['none']               = 'Ništa';
+$lang['optional']           = 'Izbor';
+$lang['complete']           = 'Gotovo';
+$lang['go_back']            = 'Idi natrag';
+$lang['saved']              = 'Spremljeno';
+
+$lang['server']             = 'Server';
+$lang['username']           = 'Korisničko ime';
+$lang['password']           = 'Lozinka';
+$lang['newpassword']        = 'Nova lozinka';
+$lang['newpassword_conf']   = '<b>Nova Lozinka</b> (potvrdi)';
+$lang['chpassword']         = 'Promjeni Lozinku';
+$lang['cur_password']       = 'Trenutna Lozinka';
+$lang['network']            = 'Mreža';
+$lang['online']             = 'Online';
+$lang['offline']            = 'Offline';
+$lang['connect']            = 'Spajanje';
+$lang['startup']            = 'Pokreni';
+$lang['files']              = 'Datoteke';
+$lang['command']            = 'Komande';
+$lang['local_dir']          = 'Lokalni Direktorij';
+$lang['working_dir']        = 'Radni Direktorij';
+$lang['pid_file']           = 'PID Datoteka';
+$lang['ip']                 = 'IP Adresa';
+$lang['ips']                = 'IP Adrese';
+$lang['port']               = 'Port';
+$lang['login']              = 'Prijava';
+$lang['logout']             = 'Odjava';
+$lang['logged_out']         = 'Uspješno si se odjavio';
+$lang['install']            = 'Instalacija';
+$lang['installing']         = 'Instalacija u tijeku';
+$lang['not_installed']      = 'Instalacija neuspjela!';
+
+$lang['unknown']            = 'Nepoznato';
+$lang['click_here']         = 'Klikni ovdje';
+$lang['documentation']      = 'GamePanelX Dokumentacija';
+$lang['update_cmd']         = 'Nadogradi CMD';
+$lang['banned_start']       = 'Zabranjene Startup Vrijednosti';
+$lang['banned_start_desc']  = '<b>NAPOMENA:</b> Stavite sve znakove koje ne želite da klijenti upisuju u prostor "vrijednosti" kao dio svog pokretačkog urednika.';
+$lang['plugin']             = 'Plugin';
+$lang['plugins']            = 'Plugins';
+$lang['del_install']        = 'Molim vas da pobrišete "install" direktorij prije nastavka!';
+$lang['version']            = 'Verzija';
+$lang['system_update']      = 'Nova nadogradnja je dostupna!';
+$lang['invalid_login']      = 'Netočna prijava! Pokušaj opet';
+$lang['permissions']        = 'Dopuštenja';
+
+
+$lang['err_query']          = 'Neuspjelo slanje upita baze podataka.';
+$lang['err_sql_update']     = 'Neuspjela narogradnja baze podataka.';
+
+
+$lang['home']               = 'Naslovnica';
+$lang['setup']              = 'Postavljanje';
+$lang['settings']           = 'Postavke';
+$lang['game_setups']        = 'Postava igre';
+$lang['cloud_games']        = 'Oblak igara';
+$lang['server_templates']   = 'Server predlošci';
+$lang['template']           = 'Predložak';
+$lang['templates']          = 'Predlošci';
+$lang['servers']            = 'Serveri';
+$lang['all_servers']        = 'Svi serveri';
+$lang['game_servers']       = 'Game Serveri';
+$lang['voice_servers']      = 'Voice Serveri';
+$lang['create_server']      = 'Kreiraj server';
+$lang['accounts']           = 'Računi';
+$lang['admins']             = 'Administratori';
+$lang['resellers']          = 'Preprodavači';
+$lang['list_users']         = 'Lista koristnika';
+$lang['add_user']           = 'Dodaj korisnika';
+$lang['list_admins']        = 'Lista administratora';
+$lang['add_admin']          = 'Dodaj administratora';
+$lang['list_resellers']     = 'Lista preprodavača';
+$lang['add_reseller']       = 'Dodaj preprodavača';
+$lang['welcome_msg']        = 'Dobrodošli na GamePanelX';
+$lang['int_name']           = 'Unutarnje ime';
+$lang['int_name_desc']      = 'Unutarnje ime mora sadržavati samo slova, brojeve i povlake, primjer "ime_2"';
+$lang['query_engine']       = 'Query Engine';
+$lang['create_network']     = 'Stvaranje mrežnog poslužitelja';
+
+$lang['delete_tp']          = 'Obriši ovaj predložak';
+$lang['create_tp']          = 'Kreiraj predložak';
+$lang['file_path']          = 'Putanja datoteke';
+$lang['browse']             = 'Traži';
+
+$lang['network_server']     = 'Mrežni poslužitelj';
+$lang['os']                 = 'Operativni sustav';
+$lang['location']           = 'Lokacija';
+$lang['datacenter']         = 'Datacentar';
+$lang['local']              = 'Mjesni';
+$lang['local_server']       = 'Mjesni Server';
+$lang['remote_server']      = 'Daljinski Server';
+$lang['no_enc_key']         = 'Ključ za šifriranje nije pronađen!  Provjeri "/configuration.php".';
+$lang['login_user']         = 'Login korisnik';
+$lang['login_pass']         = 'Login lozinka';
+$lang['login_port']         = 'Login port';
+$lang['login_homedir']      = 'Lokalni direktorij';
+$lang['net_showing_ips']    = 'Prikaz IP adrese na mreži server';
+$lang['srv_using_net']      = 'Postoje game serveri koji koriste ovu mrežu, pobrišite game servere prije.';
+$lang['add_ip']             = 'Dodaj IP adresu';
+$lang['new_ip']             = 'Nova IP adresa';
+$lang['ip_exists']          = 'Nažalost ta IP adresa već postoji!';
+$lang['ip_port_used']       = 'Nažalost kombinacija te IP adrese i Porta već postoji!';
+$lang['srv_using_ip']       = 'Postoje game serveri koji koriste ovu IP adresu, pobrišite game servere prije.';
+$lang['invalid_ip']         = 'Netočna IP adresa, provjerite dali ste dobro upisali IP adresu i pokušajte ponovno.';
+
+$lang['create_sv']          = 'Kreiraj Server';
+$lang['invalid_port']       = 'Netočan unos porta! Provjerite dali ste dobro upisali port i pokušajte ponovno.';
+$lang['invalid_intname']    = 'Netočno unutarnje ime! Koristite samo slova, brojeve i povlake u unutarnjem imenu.';
+$lang['item']               = 'Stvar';
+$lang['value']              = 'Vrijednost';
+$lang['user_editable']      = 'Korisničko uređivanje';
+$lang['restart']            = 'Resetiraj';
+$lang['stop']               = 'Zaustavi';
+$lang['update']             = 'Nadogradi';
+$lang['map']                = 'Karta';
+$lang['hostname']           = 'Hostname';
+$lang['players']            = 'Igrača';
+$lang['show_options']       = 'Prikaži sve opcije';
+$lang['simple']             = 'Jednostavno';
+$lang['advanced']           = 'Napredno';
+
+$lang['cloud_avail']        = 'Igre dostupne putem <i>gpx oblaka</i>';
+$lang['cloud_topmsg']       = 'Što se više igara doda u GamePanelX oblak, ovdje će postati dostupne.';
+
+$lang['games_add_desc']     = 'Koristite ovu formu kako bi dodali novu podršku igre.  Nakon toga možete nastaviti sa stvaranjem predloška za ovu igru.';
+$lang['games_up_icon']      = '<b>NAPOMENA:</b> Učitaj svoju 64x64 PNG ikonu';
+$lang['note_steam_auto']    = '<b>NAPOMENA:</b> Za Steam-based igre, ostavite datotečnu putanju praznu';
+
+$lang['new_filename']       = 'Novo ime podatka';
+$lang['new_dirname']        = 'Novo ime direktorija';
+
+$lang['access_ftp']         = 'Pristupi FTP';
+$lang['update_usr_det']     = 'Nadogradi korisničke informacije';
+$lang['user_exists']        = 'Nažalost, to korisničko ime je zauzeto!';
+
+$lang['def_adm_step']       = 'Korak';
+$lang['def_adm_tip_docs']   = 'Molimo vas da pročitate cijelu dokumentaciju';
+$lang['def_adm_tip_accts']  = 'Nije pronađeno! Kreirajte korisnički profil kako bi mogli kreirati game server.';
+$lang['def_adm_tip_net']    = 'Nema niti jednog mrežnog poslužitelja! Morate kreirati jedan kako bi mogli kreirati game server.';
+$lang['def_adm_tip_tpl']    = 'Nema niti jednog gotovog predloška! Za stvaranje poslužitelja morate odabrati igru i';
+$lang['def_adm_tip_srv1']   = 'Spremni ste kreirati server';
+$lang['def_adm_tip_srv2']   = 'Dovršite zadane korake kako bi kreirali game/voice server.';
+
+$lang['api_key']            = 'API Ključ';
+
+$lang['install_mirrors']    = 'Instaliraj Mirrors';
+$lang['game_panel']         = 'Game Kontrolni Panel';
+$lang['show_console_out']   = 'Kliknite za prikaz konzole izlaza';
+$lang['config_file']        = 'Config Datoteka';
+$lang['modified']           = 'Promjenjeno';
+$lang['accessed']           = 'Pristupljeno';
+$lang['size']               = 'Veličina';
+$lang['maxplayers']         = 'Maximalno igrača';
+$lang['hostname']           = 'Hostname';
+
+?>
---
+++ languages/polish.php
@@ -0,0 +1,217 @@
+<?php
+/*
+ * GamePanelX
+ * 
+ * Polish Language file
+ * Written by Mateusz880
+ * WebSite  Radaczewo24.pl
+ * Email mateusz880@radaczewo24.pl
+ * 
+ * 
+*/
+$lang = array();
+
+########################################################################
+
+// Common Words/Phrases
+$lang['yes']                = 'Tak';
+$lang['no']                 = 'Nie';
+$lang['active']             = 'Aktywny';
+$lang['inactive']           = 'Nieaktywny';
+$lang['setup']              = 'Instalacja';
+$lang['settings']           = 'Ustawienia';
+$lang['language']           = 'Język';
+$lang['default_language']   = 'Domyślny Język';
+$lang['email_address']      = 'Adres E-mail';
+$lang['company']            = 'Company';
+$lang['theme']              = 'Skórka';
+$lang['save']               = 'Zapisz';
+$lang['game']               = 'Gry';
+$lang['voice']              = 'Głosowy';
+$lang['desc']               = 'Opis';
+$lang['status']             = 'Status';
+$lang['manage']             = 'Zarządzaj';
+$lang['info']               = 'Info';
+$lang['owner']              = 'Właściciel';
+$lang['date_added']         = 'Data Dodania';
+$lang['last_updated']       = 'Edytowany';
+$lang['default']            = 'Domyślny';
+$lang['delete']             = 'Usuń';
+$lang['create']             = 'Stwórz';
+$lang['add']                = 'Dodaj';
+$lang['edit']               = 'Edytuj';
+$lang['failed']             = 'Błąd';
+$lang['name']               = 'Nazwa';
+$lang['first_name']         = 'Imię';
+$lang['last_name']          = 'Nazwisko';
+$lang['type']               = 'Typ';
+$lang['none']               = 'Brak';
+$lang['optional']           = 'Nieobowiązkowy';
+$lang['complete']           = 'Zakończono';
+$lang['go_back']            = 'Cofnij';
+$lang['saved']              = 'Zapisz';
+
+// Tech Words
+$lang['server']             = 'Serwer';
+$lang['username']           = 'Użytkownik';
+$lang['password']           = 'Hasło';
+$lang['newpassword']        = 'Nowe hasło';
+$lang['newpassword_conf']   = '<b>Nowe hasło</b> (powtórz)';
+$lang['chpassword']         = 'Zmiana hasła';
+$lang['cur_password']       = 'Current Password';
+$lang['network']            = 'Adres IP';
+$lang['online']             = 'Włączony';
+$lang['offline']            = 'Wyłączony';
+$lang['connect']            = 'IP i Port';
+$lang['startup']            = 'Komenda Startowa';
+$lang['files']              = 'Pliki';
+$lang['command']            = 'Komenda';
+$lang['local_dir']          = 'Lokalny Katalog';
+$lang['working_dir']        = 'Katalog Roboczy';
+$lang['pid_file']           = 'Plik PID';
+$lang['ip']                 = 'Adres IP';
+$lang['ips']                = 'Adresy IP';
+$lang['port']               = 'Port';
+$lang['login']              = 'Login';
+$lang['logout']             = 'Wyloguj';
+$lang['logged_out']         = 'Pomyślnie wylogowano';
+$lang['install']            = 'Instaluj';
+$lang['installing']         = 'Instaluje';
+$lang['not_installed']      = 'Nie Zainstalowano';
+
+$lang['unknown']            = 'Nieznany';
+$lang['click_here']         = 'Kliknij tu';
+$lang['documentation']      = 'GamePanelX Dokumentacja';
+$lang['update_cmd']         = 'Update CMD';
+$lang['banned_start']       = 'Banned Startup Values';
+$lang['banned_start_desc']  = '<b>Note:</b> Put all characters you do not want clients to type in the "value" part of their startup editor.';
+$lang['plugin']             = 'Plugin';
+$lang['plugins']            = 'Pluginy';
+$lang['del_install']        = 'Usuń katalog "install" aby kontynuować!';
+$lang['version']            = 'Wersja';
+$lang['system_update']      = 'A system update is available!';
+$lang['invalid_login']      = 'Sesja wygasła!  Zaloguj się ponownie.';
+$lang['permissions']        = 'Uprawnienia';
+
+// Error messages
+$lang['err_query']          = 'Failed to query the database';
+$lang['err_sql_update']     = 'Failed to update the database';
+
+// Left Panel
+$lang['home']               = 'Start';
+$lang['setup']              = 'Ustawienia';
+$lang['settings']           = 'Konfiguracja';
+$lang['game_setups']        = 'Ustawienia Gier';
+$lang['cloud_games']        = 'Cloud Games';
+$lang['server_templates']   = 'Server Templates';
+$lang['template']           = 'Template';
+$lang['templates']          = 'Templates';
+$lang['servers']            = 'Serwery';
+$lang['all_servers']        = 'Wszystkie Serwery';
+$lang['game_servers']       = 'Serwery Gry';
+$lang['voice_servers']      = 'Serwery Głosowe';
+$lang['create_server']      = 'Stwórz serwer';
+$lang['accounts']           = 'Konta';
+$lang['admins']             = 'Admin';
+$lang['resellers']          = 'Resellers';
+$lang['list_users']         = 'Lista Użytkowników';
+$lang['add_user']           = 'Dodaj użytkownika';
+$lang['list_admins']        = 'Lista Adminów';
+$lang['add_admin']          = 'Dodaj Admina';
+$lang['list_resellers']     = 'Lista Resellers';
+$lang['add_reseller']       = 'Dodaj Resellera';
+$lang['welcome_msg']        = 'Witam w  GamePanelX';
+$lang['int_name']           = 'Internal Name';
+$lang['int_name_desc']      = 'Internal Name should only contain letters, numbers and underscores, such as "red_1"';
+$lang['query_engine']       = 'Silnik Query';
+$lang['create_network']     = 'Dodaj serwer sieci';
+
+// Templates
+$lang['delete_tp']          = 'Usuń Template';
+$lang['create_tp']          = 'Stwórz Template';
+$lang['file_path']          = 'Położenie plików';
+$lang['browse']             = 'Przeglądaj';
+
+// Network
+$lang['network_server']     = 'Network Server';
+$lang['os']                 = 'System Operacyjny';
+$lang['location']           = 'Lokalizacja';
+$lang['datacenter']         = 'Datacenter';
+$lang['local']              = 'Local';
+$lang['local_server']       = 'Serwer Lokalny';
+$lang['remote_server']      = 'Serwer Zdalny';
+$lang['no_enc_key']         = 'Nie znaleziono klucz szyfrowania!  Sprawdź "/configuration.php".';
+$lang['login_user']         = 'Login';
+$lang['login_pass']         = 'Hasło';
+$lang['login_port']         = 'Port Logowania';
+$lang['login_homedir']      = 'Katalog Domowy';
+$lang['net_showing_ips']    = 'Wywietlanie adresów IP na serwerze sieciowym';
+$lang['srv_using_net']      = 'There are game servers using this Network Server!  Delete the servers first and try again.';
+$lang['add_ip']             = 'Dodaj Adres IP';
+$lang['new_ip']             = 'Nowy Adres IP';
+$lang['ip_exists']          = 'Przykro nam, ale adres IP istnieje już!';
+$lang['ip_port_used']       = 'Przykro nam, ale kombinacji IP/port jest już w użyciu!';
+$lang['srv_using_ip']       = 'Istnieją serwery z wykorzystaniem tego adresu IP!  Usuń istniejący serwer i spróbuj ponownie.';
+$lang['invalid_ip']         = 'Nepoprawny Adres IP!  Sprawdź i spróbuj ponownie.';
+
+// Servers
+$lang['create_sv']          = 'Stwórz Serwer';
+$lang['invalid_port']       = 'Nie poprawny port!  Skróbuj ponownie.';
+$lang['invalid_intname']    = 'Invalid Internal Name specified!  Only letters, numbers, - and _ are accepted.  Please try again.';
+$lang['item']               = 'Item';
+$lang['value']              = 'Value';
+$lang['user_editable']      = 'User Editable';
+$lang['restart']            = 'Restart';
+$lang['stop']               = 'Stop';
+$lang['update']             = 'Aktualizuj';
+$lang['map']                = 'Mapa';
+$lang['hostname']           = 'Nazwa serwera';
+$lang['players']            = 'Gracz';
+$lang['show_options']       = 'Pokaż więcej opcji';
+$lang['simple']             = 'Prosty';
+$lang['advanced']           = 'Zawansowany';
+
+// Cloud
+$lang['cloud_avail']        = 'Games Available via <i>gpx cloud</i>';
+$lang['cloud_topmsg']       = 'As more games are added to the GamePanelX Cloud, they will become available here.';
+
+// Games
+$lang['games_add_desc']     = 'Use this form to add new game support.  You can then proceed with creating a template for this game.';
+$lang['games_up_icon']      = '<b>Note:</b> Upload your 64x64 PNG icon to';
+$lang['note_steam_auto']    = '<b>Note:</b> For Steam-based games, leave the File Path empty to use the Steam auto-installer';
+
+// File Manager
+$lang['new_filename']       = 'Nazwa pliku';
+$lang['new_dirname']        = 'Nazwa Folderu';
+
+// User perms
+$lang['access_ftp']         = 'Dostęp FTP';
+$lang['update_usr_det']     = 'Aktualizuj dane użytkownika';
+$lang['user_exists']        = 'Przykro nam, ale ta nazwa już istnieje!';
+
+// Home Page hints
+$lang['def_adm_step']       = 'Krok';
+$lang['def_adm_tip_docs']   = 'Proszę zapoznać się z pełną dokumentacją';
+$lang['def_adm_tip_accts']  = 'Brak kont ! Należy najpierw stworzyć konto użytkownika, aby utworzyć serwer';
+$lang['def_adm_tip_net']    = 'Brak Serwerów. Należy utworzyć je teraz';
+$lang['def_adm_tip_tpl']    = 'No completed templates found!  To create servers, you should pick a game and';
+$lang['def_adm_tip_srv1']   = 'You are all ready to create a server!';
+$lang['def_adm_tip_srv2']   = 'Complete the steps above to create a game/voice server.';
+
+// Other
+$lang['api_key']            = 'Klucz API';
+
+##############################################################################################################
+
+// 3.0.8
+$lang['install_mirrors']    = 'Install Mirrors';
+$lang['game_panel']         = 'Game Control Panel';
+$lang['show_console_out']   = 'Kliknij aby zobaczyć konsole';
+$lang['config_file']        = 'Plik Konfiguracji';
+$lang['modified']           = 'Zmodyfikany';
+$lang['accessed']           = 'Utworzony';
+$lang['size']               = 'Rozmiar';
+$lang['maxplayers']         = 'Max Graczy';
+$lang['hostname']           = 'Nazwa serwera';
+
+?>
