cc7ea49203e877905373cef589422175ff7957f8
Ricky Burgin <ricky-github@burg.in>
2013-07-31 18:51:48 +0100

    Added Japanese language support

---
+++ languages/japanese.php
@@ -0,0 +1,218 @@
+﻿<?php
+/*
+ * GamePanelX
+ * 
+ * Japanese Language file
+ * Written by Ricky Burgin (Orbixx) ricky@‎burg.in
+ *
+ * 日本語ファイル
+ * 翻訳者：　バーギン・リッキー (ユーザー：　Orbixx) メール：　ricky@‎burg.in
+ * 
+ * 
+*/
+$lang = array();
+
+########################################################################
+
+// Common Words/Phrases
+$lang['yes']                = 'はい';
+$lang['no']                 = 'いいえ';
+$lang['active']             = 'アクティブ';
+$lang['inactive']           = '非アクティブ';
+$lang['setup']              = 'セットアップ';
+$lang['settings']           = '設定';
+$lang['language']           = '言語';
+$lang['default_language']   = 'デフォルトの言語';
+$lang['email_address']      = 'メールアドレス';
+$lang['company']            = '会社';
+$lang['theme']              = 'テーム';
+$lang['save']               = 'セーブ';
+$lang['game']               = 'ゲーム';
+$lang['voice']              = 'ボイス';
+$lang['desc']               = '説明';
+$lang['status']             = 'ステイタス';
+$lang['manage']             = '管理';
+$lang['info']               = '情報';
+$lang['owner']              = '所有者（オーナー）';
+$lang['date_added']         = '追加された日付';
+$lang['last_updated']       = '最終更新';
+$lang['default']            = 'デフォルト';
+$lang['delete']             = '削除';
+$lang['create']             = '作る';
+$lang['add']                = '加える';
+$lang['edit']               = '編集';
+$lang['failed']             = '失敗しました';
+$lang['name']               = 'ネーム';
+$lang['first_name']         = '名前';
+$lang['last_name']          = '名字';
+$lang['type']               = 'タイプ';
+$lang['none']               = '無し';
+$lang['optional']           = '任意';
+$lang['complete']           = '完了';
+$lang['go_back']            = '戻る';
+$lang['saved']              = 'セーブしました';
+
+// Tech Words
+$lang['server']             = 'サーバー';
+$lang['username']           = 'ユーザー';
+$lang['password']           = 'パスワード';
+$lang['newpassword']        = '新しいのパスワード';
+$lang['newpassword_conf']   = '<b>新しいのパスワード</b> (確認)';
+$lang['chpassword']         = 'パスワードを変更する';
+$lang['cur_password']       = '現在のパスワード';
+$lang['network']            = 'ネットワーク';
+$lang['online']             = 'オンライン';
+$lang['offline']            = 'オフライン';
+$lang['connect']            = '接続';
+$lang['startup']            = 'スタートアップ';
+$lang['files']              = 'ファイル';
+$lang['command']            = '命令';
+$lang['local_dir']          = 'ローカルディレクトリ';
+$lang['working_dir']        = '今のディレクトリ';
+$lang['pid_file']           = 'PID ファイル';
+$lang['ip']                 = 'IPアドレス';
+$lang['ips']                = '複数のIPアドレス';
+$lang['port']               = 'ポート';
+$lang['login']              = 'ログイン';
+$lang['logout']             = 'ログアウト';
+$lang['logged_out']         = 'ログアウトしました';
+$lang['install']            = 'インストール';
+$lang['installing']         = 'インストール中';
+$lang['not_installed']      = 'インストールされていません';
+
+$lang['unknown']            = '不明';
+$lang['click_here']         = 'ここをクリックして';
+$lang['documentation']      = 'GamePanelXのドキュメンテーション';
+$lang['update_cmd']         = 'CMDをアップデートする';
+$lang['banned_start']       = '禁止されたスタートアップの変数';
+$lang['banned_start_desc']  = '<b>お知らせ:</b>お客様が入力しないように、禁止の文字を入れてください';
+$lang['plugin']             = 'プラグイン';
+$lang['plugins']            = 'プラグインズ';
+$lang['del_install']        = '続くの前に、"install"って言うのディレクトリを消してください';
+$lang['version']            = 'バージョン';
+$lang['system_update']      = '新しいのシステムのバージョンがあります！';
+$lang['invalid_login']      = 'ログインは失敗しました、もう一度やり直してください';
+$lang['permissions']        = '許可';
+
+// Error messages
+$lang['err_query']          = 'データベースを照会が失敗しました';
+$lang['err_sql_update']     = 'データベースをアップデートが失敗しました';
+
+// Left Panel
+$lang['home']               = 'ホーム';
+$lang['setup']              = 'セットアップ';
+$lang['settings']           = '設定';
+$lang['game_setups']        = 'ゲームのセットアップ';
+$lang['cloud_games']        = 'クラウドのゲーム';
+$lang['server_templates']   = 'サーバーのテンプレート';
+$lang['template']           = 'テンプレート';
+$lang['templates']          = 'テンプレーツ';
+$lang['servers']            = 'サーバーズ';
+$lang['all_servers']        = '全部のサーバー';
+$lang['game_servers']       = 'ゲームのサーバー';
+$lang['voice_servers']      = 'ボイスのサーバー';
+$lang['create_server']      = 'サーバーを作る';
+$lang['accounts']           = 'アカウント';
+$lang['admins']             = '管理者';
+$lang['resellers']          = '再販業者';
+$lang['list_users']         = 'ユーザーのリスト';
+$lang['add_user']           = 'ユーザーの追加';
+$lang['list_admins']        = '管理者のリスト';
+$lang['add_admin']          = '管理者の追加';
+$lang['list_resellers']     = '再販業者のリスト';
+$lang['add_reseller']       = '再販業者の追加';
+$lang['welcome_msg']        = 'GamePanelXへようこそ';
+$lang['int_name']           = '内部名';
+$lang['int_name_desc']      = '内部名には、文字と数字と下線だけが入られます';
+$lang['query_engine']       = 'クエリーエンジン';
+$lang['create_network']     = 'ネットワーク・サーバーを作る';
+
+// Templates
+$lang['delete_tp']          = 'このテンプレートを削除する';
+$lang['create_tp']          = 'テンプレートを作る';
+$lang['file_path']          = 'ファイルパス';
+$lang['browse']             = 'ブラウズ';
+
+// Network
+$lang['network_server']     = 'ネットワーク・サーバー';
+$lang['os']                 = 'OS';
+$lang['location']           = '場所';
+$lang['datacenter']         = 'データセンター';
+$lang['local']              = 'ローカル';
+$lang['local_server']       = 'ローカルサーバー';
+$lang['remote_server']      = 'リモートサーバー';
+$lang['no_enc_key']         = '暗号化キーは見つけれなかった、/configuration.phpをチェックしてください';
+$lang['login_user']         = 'ユーザー';
+$lang['login_pass']         = 'パスワード';
+$lang['login_port']         = 'ポート';
+$lang['login_homedir']      = 'ホームディレクトリ';
+$lang['net_showing_ips']    = 'ネットワークサーバーのIPアドレスを表示しています';
+$lang['srv_using_net']      = 'ゲームサーバーはこのネットワーク・サーバーを使用されています！ゲームサーバを削除してもう一度やり直してください';
+$lang['add_ip']             = 'IPアドレスを追加';
+$lang['new_ip']             = '新しいのIPアドレス';
+$lang['ip_exists']          = 'そのIPアドレスは使用中です';
+$lang['ip_port_used']       = 'そのIPアドレスとポートは使用中です';
+$lang['srv_using_ip']       = 'あるゲームサーバーはこのIPアドレスを使っていますのでサーバーを削除してやり直してください';
+$lang['invalid_ip']         = 'IPアドレスは無効なのでチェックしてやり直してください';
+
+// Servers
+$lang['create_sv']          = 'サーバーを作る';
+$lang['invalid_port']       = 'ポートは無効なのでチェックしてやり直してください';
+$lang['invalid_intname']    = '内部名は無効なのでチェックしてやり直してください';
+$lang['item']               = 'アイテム';
+$lang['value']              = '変数値';
+$lang['user_editable']      = 'ユーザーは編集可能';
+$lang['restart']            = 'リスタート';
+$lang['stop']               = 'ストップ';
+$lang['update']             = 'アップデート';
+$lang['map']                = '地図';
+$lang['hostname']           = 'ホストネーム';
+$lang['players']            = 'プレイヤー';
+$lang['show_options']       = 'せべてのオプションを表示する';
+$lang['simple']             = '簡単';
+$lang['advanced']           = '高度';
+
+// Cloud
+$lang['cloud_avail']        = '<i>gpx cloud</i>で利用できるゲーム';
+$lang['cloud_topmsg']       = ''; // Unnecessary
+
+// Games
+$lang['games_add_desc']     = '新しいのゲームのサポートを追加ようにこのフォームをご使用してください。それで、このゲームはテンプレートを作って続けます。';
+$lang['games_up_icon']      = '<b>Note:</b> 64x64のPNGのアイコンをアップロード';
+$lang['note_steam_auto']    = '<b>Note:</b> SteamかValveのゲームですなら「ファイルパス」空のフィールドにしてください';
+
+// File Manager
+$lang['new_filename']       = '新しいのファイルネーム';
+$lang['new_dirname']        = '新しいのディレクトリ';
+
+// User perms
+$lang['access_ftp']         = 'FTPをアクセスする';
+$lang['update_usr_det']     = 'ユーザーの詳細をアップデートする';
+$lang['user_exists']        = 'そのユーザーはもう存在していますので他のユーザーを選んで下さい';
+
+// Home Page hints
+$lang['def_adm_step']       = 'ステップ';
+$lang['def_adm_tip_docs']   = 'ドキュメンテーションをご覧ください';
+$lang['def_adm_tip_accts']  = 'アカウントがありません、サーバーを作るようにユーザーを作るべき';
+$lang['def_adm_tip_net']    = 'ネットワーク・サーバーがありません、今すぐ作るべき';
+$lang['def_adm_tip_tpl']    = '完了したのテンプレートがありません、サーバーを作るように、ゲームを選んで';
+$lang['def_adm_tip_srv1']   = 'サーバーを作るの準備が出来ました！';
+$lang['def_adm_tip_srv2']   = 'ゲームかボイスのサーバーを作るように、上の指示に従ってください。';
+
+// Other
+$lang['api_key']            = 'APIキー';
+
+##############################################################################################################
+
+// 3.0.8
+$lang['install_mirrors']    = 'ミラーをインストールする';
+$lang['game_panel']         = 'ゲームのコントロールパネル';
+$lang['show_console_out']   = 'コンソールを表示されるようにクリックしてください';
+$lang['config_file']        = 'コンフィッグ・ファイル';
+$lang['modified']           = '修正された';
+$lang['accessed']           = 'アクセスされた';
+$lang['size']               = 'サイズ';
+$lang['maxplayers']         = '最大のプレイヤー';
+$lang['hostname']           = 'ホストネーム';
+
+?>
